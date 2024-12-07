7fe3fcc3e3b5e3dd59dcd17b8ef9538b3c9106f7
Piotrek Witkowski <piotrekwitkowski1@gmail.com>
2024-12-07 22:58:09 +0000

    Update local-sharp

--- local-sharp/index.ts
+++ local-sharp/index.ts
@@ -6,9 +6,38 @@ const width = 600;
 const format = 'avif';
 
 const image = sharp(file).resize(width).toFormat(format);
-sharp.cache({ items: 50 });
+sharp.cache(false);
 
-for (let i = 0; i < 20; i++) {
-  console.log(process.memoryUsage());
-  await image.toFile('/dev/null');
+
+let start, end;
+
+start = performance.now();
+for (let i = 0; i < 2; i++) {
+  console.log(i);
+  // console.log(process.memoryUsage());
+  console.log(await image.toFile('/dev/null'));
 }
+end = performance.now();
+console.log('diff:', end - start);
+
+
+start = performance.now();
+for (let i = 0; i < 2; i++) {
+  console.log(i);
+  // console.log(process.memoryUsage());
+  console.log(await image.toFile('/tmp/sharp_out'));
+}
+end = performance.now();
+console.log('diff:', end - start);
+
+
+start = performance.now();
+for (let i = 0; i < 20; i++) {
+  console.log(i);
+  // console.log(process.memoryUsage());
+  await image.toBuffer();
+}
+end = performance.now();
+console.log('diff:', end - start);
+
+
